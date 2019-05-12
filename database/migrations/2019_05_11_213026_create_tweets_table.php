8db8925bf73a3b78173dda3c58f6b9cbc5647d9c
yidongyi8@gmail.com <otbs1016>
2019-05-12 10:48:00 -0700

    first commit

---
+++ database/migrations/2019_05_11_213026_create_tweets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('content');
+            $table->datetime('tweet_time');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/custom.css">
+  <title>tsubastagram</title>
+</head>
+<body>
+  <header class="otbs-header inner">
+    <div class="otbs-header__title">tsubastagram</div>
+    <ul class="otbs-header__list">
+      <li class="otbs-header__list__item"><a href="#">about</a></li>
+      <li class="otbs-header__list__item"><a href="#">about</a></li>
+      <li class="otbs-header__list__item"><a href="#">about</a></li>
+      <li class="otbs-header__list__item"><a href="#">about</a></li>
+      <li class="otbs-header__list__item"><a href="#">about</a></li>
+    </ul>
+  </header>
+</body>
+</html>
